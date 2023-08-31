72a79eafa4e80abe631c63bbe20cd2dbee005151
Brandon Dykun <85542666+brandondykun@users.noreply.github.com>
2023-08-31 08:52:07 -0400

    Added mobile nav menu. (#1)
    * Added mobile nav menu, updated formatting configuration.
    
    ---------
    
    Co-authored-by: Brandon Dykun <brandon.dykun@barnstormresearch.com>
    Co-authored-by: Rem Wransky <13735131+RWransky@users.noreply.github.com>

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,32 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
+import hamburgerMenu from "../../public/hamburger-menu.svg";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const links = [
+  { path: "/", text: "About" },
+  { path: "/publications", text: "Publications" },
+  { path: "/contact", text: "Contact Us" },
+  { path: "/join", text: "Join Our Team" },
+];
 
 type NavLinkProps = {
   href: string;
   text: string;
+  onClick?: React.MouseEventHandler<HTMLSpanElement>;
 };
 
-const NavLink = ({ href, text }: NavLinkProps) => {
+const NavLink = ({ href, text, onClick }: NavLinkProps) => {
   const currentRoute = usePathname();
 
   return (
@@ -18,33 +36,54 @@ const NavLink = ({ href, text }: NavLinkProps) => {
         currentRoute === href ? "underline" : ""
       }`}
     >
-      {text}
+      {onClick ? <span onClick={onClick}>{text}</span> : text}
     </Link>
   );
 };
 
 const Navbar = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <div className="bg-background">
+    <div className="bg-background flex justify-end items-center p-4">
       {/* Desktop Nav */}
-      <nav>
-        <ul className="hidden sm:flex gap-6 p-4 justify-center">
-          <NavLink href="/" text="About" />
-          <NavLink href="/publications" text="Publications" />
-          <NavLink href="/contact" text="Contact" />
-          <NavLink href="/join" text="Join Our Team" />
+      <nav className="hidden sm:block">
+        <ul className="flex gap-6 md:gap-12 justify-center">
+          {links.map((link) => {
+            return (
+              <NavLink href={link.path} text={link.text} key={link.text} />
+            );
+          })}
         </ul>
       </nav>
 
       {/* Mobile nav */}
-      <nav>
-        <ul className="flex flex-col gap-2 p-4 sm:hidden">
-          <NavLink href="/" text="About" />
-          <NavLink href="/publications" text="Publications" />
-          <NavLink href="/contact" text="Contact" />
-          <NavLink href="/join" text="Join Our Team" />
-        </ul>
-      </nav>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger className="sm:hidden">
+          <Image
+            src={hamburgerMenu as string}
+            alt="Menu."
+            className="h-6 w-6"
+          />
+        </SheetTrigger>
+        <SheetContent side="top">
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+            <nav className="flex flex-col gap-2">
+              {links.map((link) => {
+                return (
+                  <NavLink
+                    key={link.text}
+                    href={link.path}
+                    text={link.text}
+                    onClick={() => setOpen(false)}
+                  />
+                );
+              })}
+            </nav>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
