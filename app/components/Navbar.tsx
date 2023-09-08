4da4e942c08256e16f98aee2eb48e09615d4e8c5
Brandon Dykun <brandon.dykun@barnstormresearch.com>
2023-09-08 10:24:31 -0400

    Modified navbar styling.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,29 +14,66 @@ import {
 } from "@/components/ui/sheet";
 
 const links = [
-  { path: "/", text: "About" },
+  { path: "/#about", text: "About" },
   { path: "/publications", text: "Publications" },
   { path: "/contact", text: "Contact Us" },
   { path: "/join", text: "Join Our Team" },
 ];
 
-type NavLinkProps = {
-  href: string;
-  text: string;
-  onClick?: React.MouseEventHandler<HTMLSpanElement>;
+const getActiveRouteBool = (currentRoute: string, href: string) => {
+  return (
+    currentRoute.includes(href.slice(1)) ||
+    (currentRoute === "/" && href === "/#about")
+  );
 };
 
-const NavLink = ({ href, text, onClick }: NavLinkProps) => {
+type HeaderNavLinkProps = {
+  href: string;
+  text: string;
+};
+
+const HeaderNavLink = ({ href, text }: HeaderNavLinkProps) => {
   const currentRoute = usePathname();
+
+  const isActiveRoute = getActiveRouteBool(currentRoute, href);
 
   return (
     <Link
       href={href}
-      className={`text-stone-900 hover:text-stone-600 w-fit ${
-        currentRoute === href ? "underline" : ""
-      }`}
+      className={`${
+        isActiveRoute
+          ? " text-neutral-200 underline underline-offset-8 hover:cursor-default"
+          : "text-neutral-400 hover:text-neutral-200 border-transparent"
+      } w-fit whitespace-nowrap transition-all duration-300 py-4 px-4 text-center font-light`}
     >
-      {onClick ? <span onClick={onClick}>{text}</span> : text}
+      {text}
+    </Link>
+  );
+};
+
+type MobileNavLinkProps = {
+  href: string;
+  text: string;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+};
+
+const MobileNavLink = ({ href, text, onClick }: MobileNavLinkProps) => {
+  const currentRoute = usePathname();
+
+  const isActiveRoute = getActiveRouteBool(currentRoute, href);
+
+  return (
+    <Link
+      href={href}
+      className={`${
+        isActiveRoute
+          ? "text-neutral-200 bg-neutral-900 rounded font-semibold hover:cursor-default"
+          : "text-neutral-400 hover:text-neutral-100"
+      }   whitespace-nowrap transition-all duration-300 text-left`}
+    >
+      <div className="py-2 px-4" onClick={onClick}>
+        {text}
+      </div>
     </Link>
   );
 };
@@ -45,13 +82,17 @@ const Navbar = () => {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <div className="bg-background flex justify-end items-center p-4">
+    <div className="flex justify-end items-center px-4 bg-neutral-900 shadow-lg">
       {/* Desktop Nav */}
       <nav className="hidden sm:block">
-        <ul className="flex gap-6 md:gap-12 justify-center">
+        <ul className="flex gap-4 md:gap-10 justify-center">
           {links.map((link) => {
             return (
-              <NavLink href={link.path} text={link.text} key={link.text} />
+              <HeaderNavLink
+                href={link.path}
+                text={link.text}
+                key={link.text}
+              />
             );
           })}
         </ul>
@@ -59,20 +100,20 @@ const Navbar = () => {
 
       {/* Mobile nav */}
       <Sheet open={open} onOpenChange={setOpen}>
-        <SheetTrigger className="sm:hidden">
+        <SheetTrigger className="sm:hidden py-4">
           <Image
             src={hamburgerMenu as string}
             alt="Menu."
             className="h-6 w-6"
           />
         </SheetTrigger>
-        <SheetContent side="top">
+        <SheetContent side="right">
           <SheetHeader>
-            <SheetTitle>Menu</SheetTitle>
-            <nav className="flex flex-col gap-2">
+            <SheetTitle className="text-neutral-300">Menu</SheetTitle>
+            <nav className="flex flex-col gap-3">
               {links.map((link) => {
                 return (
-                  <NavLink
+                  <MobileNavLink
                     key={link.text}
                     href={link.path}
                     text={link.text}
--- tailwind.config.js
+++ tailwind.config.js
@@ -38,4 +38,7 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
 };
