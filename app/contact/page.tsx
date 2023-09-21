b9ed6cfcf723f6fac3f73a6ebfe1a408a378d509
Rem Wransky <13735131+RWransky@users.noreply.github.com>
2023-09-21 13:49:28 -0400

    add general inquiries

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import ContactForm from "./ContactForm";
+import Image from "next/image";
+import barnstormLogo from "../../public/barnstorm_logo.jpg";
 
 export const metadata: Metadata = {
   title: "Contact Us",
@@ -24,7 +26,46 @@ const ContactPage = () => {
             <ContactForm />
           </div>
         </div>
-        <div className="tracking-[0] text-base font-normal leading-[132%] block"></div>
+        <div className="tracking-[0] text-base font-normal leading-[132%] block">
+          <div className="px-4 pb-36 pt-12">
+            <div className="relative w-fit m-auto sm:p-16 max-w-2xl">
+              <h2 className="text-center text-2xl uppercase tracking-widest pb-12 px-4 text-neutral-400 font-extralight">
+                General Inquiries
+              </h2>
+              <Image
+                src={barnstormLogo.src}
+                alt="Barnstorm Research Logo"
+                width={450}
+                height={100}
+                className="pb-16 mx-auto sm:m-0"
+              />
+              <p className="max-w-4xl m-auto text-center p-4 relative text-xl font-extralight text-neutral-700">
+                <a
+                  href="mailto: ideas@barnstormresearch.com"
+                  className="text-main-accent font-semibold"
+                >
+                  ideas@barnstormresearch.com
+                </a>
+                <br />
+                <span>Barnstorm Research Corporation</span>
+                <br />
+                <span>PO Box 484</span>
+                <br />
+                <span>Atkinson, NH 03811</span>
+
+                {/* At Barnstorm Research, we believe in the power of{" "}
+                <span className="text-main-accent font-semibold">ideas</span>,
+                and in the{" "}
+                <span className="text-main-accent font-semibold">people</span>{" "}
+                that bring them to light. */}
+              </p>
+              <div className="h-8 w-8 border border-solid border-neutral-400 top-0 left-0  absolute border-r-0 border-b-0 hidden sm:block"></div>
+              <div className="h-8 w-8 border border-solid border-neutral-400 bottom-0 right-0 absolute border-l-0 border-t-0 hidden sm:block"></div>
+              <div className="h-8 w-8 border border-solid border-neutral-400 top-0 right-0 absolute border-l-0 border-b-0 hidden sm:block"></div>
+              <div className="h-8 w-8 border border-solid border-neutral-400 bottom-0 left-0  absolute border-r-0 border-t-0 hidden sm:block"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
